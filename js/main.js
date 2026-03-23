ba428afd619e32e700c6ca59a0646cce7a65b72d
Justin Lauricio <lauriciolau902@gmail.com>
2026-03-23 23:23:23 +0800

    attach event listeners to DOM elements

--- js/main.js
+++ js/main.js
@@ -4,12 +4,28 @@
  */
 
 import { time_now } from "./time.js";
+import { find_student } from "./search.js";
+import { display_list } from "./display.js";
+
 
 // DOM Elements
 const btnShowDate = document.getElementById("show-current-date-btn");
 const dateOutput = document.getElementById("current-date-output");
+const btnSearch = document.getElementById("search-btn");
+const inputSearch = document.getElementById("search-input");
+const btnDisplayAll = document.getElementById("display-all-btn");
 
 // Show current time
 btnShowDate?.addEventListener("click", () => {
   dateOutput.innerHTML = time_now();
 });
+
+// Search student by ID
+btnSearch?.addEventListener("click", find_student);
+
+inputSearch?.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") find_student();
+});
+
+// Display all students
+btnDisplayAll?.addEventListener("click", display_list);
